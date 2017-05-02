498daa43afa45ab29fede0b979847d65fdc44b53
Mathias Schultz Rossen <mathias.rossen@gmail.com>
2017-05-02 11:26:47 +0200

    GUI for creating contract and creating customer added.

--- Skrotlog.UI/ViewModel/CreateCustomerViewModel.cs
+++ Skrotlog.UI/ViewModel/CreateCustomerViewModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Skrotlog.DAL
+namespace Skrotlog.UI.ViewModel
 {
-    public class Class1
+    class CreateCustomerViewModel
     {
     }
 }
