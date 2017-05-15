b837dce3c74f1ad954aa7359a83162e231437287
Mathias Schultz Rossen <mathias.rossen@gmail.com>
2017-05-15 10:38:54 +0200

    Test ignore

--- Skrotlog.DAL/DatabaseController.cs
+++ Skrotlog.DAL/DatabaseController.cs
@@ -11,7 +11,7 @@ namespace Skrotlog.DAL
 {
     public class DatabaseController
     {
-        //elkddl kjghsl fkjghdj shf dshfjgd shk
+        // SHOULD NOT BE COMMITTED
         MySqlConnectionStringBuilder connectionString;
 
         public DatabaseController()
